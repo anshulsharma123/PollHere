f9bb334aa99a14f3f1f4e0d72aa9d76a4ffbf48c
anshulsharma123 <anshul.sharma2019@vitbhopal.ac.in>
2021-07-21 23:24:22 +0530

    random changes

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var cors = require('cors')
 
 app.use(cors())
 //db connection
-const port=process.env.port||7000;
-app.listen(port, ()=>{
-    console.log(`listening on port ${port}`)
-});
 require('./startup/db')();
 
 //routes 
 require("./startup/routes")(app);
+const port=process.env.port||7000;
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+});
 if(process.env.NODE_ENV==="production")
 {
     app.use(express.static("client/build"));
