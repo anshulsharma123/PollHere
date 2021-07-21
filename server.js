e2b37acc3ea8d1ffa7fae79b6d63578897f937ae
anshulsharma123 <anshul.sharma2019@vitbhopal.ac.in>
2021-07-21 22:22:37 +0530

    changes made for deploying

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ require('./startup/db')();
 
 //routes 
 require("./startup/routes")(app);
-
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static("client/build"));
+    const path=require("path");
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+    })
+}
 const port=process.env.port||7000;
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
